ba5be4c5f5f70640541800cd1e674acd5d51c9a3
radiohead11 <aichengjaohai@gmail.com>
2022-03-02 03:21:55 -0800

    poststatus

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -5,18 +5,35 @@ import add from '../add.PNG'
 import explore from '../explore.PNG'
 import like from '../like.PNG'
 import style from '../styles/Header.module.css'
+import Newpost from './newpost'
+
+import {useState , useEffect} from 'react'
 // import girl from '../girl.jpg'
 
 
 const Nav = () => {
-  return (
+  const [displaystyle , setdisplaystyle] = useState({display : "none"});
+  console.log(displaystyle)
+
+
+   return (
+    
     <div className={style.navBox}>
         <img src={home} alt="icon" />
         <img src={messenger} alt="icon" />
-        <img src={add} alt="icon" />
+        <img src={add} alt="icon" onClick={()=>{
+          if(displaystyle.display === 'none'){
+          setdisplaystyle({display:'block'})}
+          if(displaystyle.display === 'block'){
+            setdisplaystyle({display:'none'})}
+          
+        }} />
         <img src={explore} alt="icon" />
         <img src={like} alt="icon" />
         {/* <img src={girl} alt="icon" /> */}
+        <div style={displaystyle}>
+        <Newpost />
+        </div>
     </div>
   )
 }
---
+++ src/component/newpost.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import style from '../styles/newpostform.module.css'
+
+const Newpost = () => {
+  return (
+    <div className={style.postbox}>
+        <form>
+            <label>
+                enter your name:
+                <input type={'text'}/>
+            </label>
+            <label>
+                enter image url:
+                <input type={'content'}/>
+            </label>
+            <button className={style.buttonsubmit}>submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default Newpost
