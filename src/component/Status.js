c7f96d07e0ea12a26b4770773cf2736bbb178ca9
Jiyang Ju Kumbang <jiyangju2016@gmail.com>
2022-03-02 11:00:42 +0530

    change in status block

--- src/component/Status.js
+++ src/component/Status.js
@@ -1,16 +1,10 @@
-
 import styles from '../styles/StatusBlock.module.css'
 
 const Status = ({item}) => {
   return (
       <div className={styles.selfProfile}>
-          <div className={styles.profilePic}>
-              <img src={item.imageUrl} alt="DP"/>
-          </div>
-          <div>
-              <p>{item.name}</p>
-          </div>
-
+        <img src={item.imageUrl} alt="DP"/>
+        <p className={styles.nameText}>{item.name}</p>
       </div>
   )
 }
