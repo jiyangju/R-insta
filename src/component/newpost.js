e5b294e5d88706008cda53875bacb1bf58c6a3c0
Jiyang Ju Kumbang <jiyangju2016@gmail.com>
2022-03-03 12:00:07 +0530

    change in post style

--- src/component/newpost.js
+++ src/component/newpost.js
@@ -3,19 +3,19 @@ import style from '../styles/newpostform.module.css'
 
 const Newpost = () => {
   return (
-    <div className={style.postbox}>
-        <form>
-            <label>
-                enter your name:
-                <input type={'text'}/>
-            </label>
-            <label>
-                enter image url:
-                <input type={'content'}/>
-            </label>
-            <button className={style.buttonsubmit}>submit</button>
-        </form>
-    </div>
+      <div className={style.postbox}>
+          <form>
+              <label>
+                  enter your name:
+                  <input type={'text'}/>
+              </label>
+              <label>
+                  enter image url:
+                  <input type={'content'}/>
+              </label>
+              <button className={style.buttonsubmit}>submit</button>
+          </form>
+      </div>
   )
 }
 
